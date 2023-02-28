8da5233ad31bdbe0a67a966cf4eace159cd7f707
Cristhian <cristhian-eduardo28@hotmail.com>
2023-02-28 15:15:59 -0800

    Cambio en el backend sobre la ruta

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.use('/api/auth', require('./routes/auth'));
 //*Ruta de eventos
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 //* Escuchar peticiones
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
